88a4607db3f59f269d983a5083930332432874eb
V <130299076+Vin1-SF19@users.noreply.github.com>
2024-10-02 13:36:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+
+var adicionar = document.getElementById('add');
+var contaItem = document.getElementById('quantidade');
+
+adicionar.addEventListener("click", function(){
+
+
+    contaItem.style.display = 'inline-block';
+
+});
